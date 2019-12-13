e3ef05176ff76c268e9f3c34ffff7723c887b4cf
jamesbond007dj <ilkerzaimoglu@gmail.com>
2019-12-13 04:44:49 -0800

    working on player bot

---
+++ player-bot.py
@@ -0,0 +1,53 @@
+from game_of_greed import Game
+
+
+class LazyPlayer:
+    # play and calculate score avaliable
+    # everything else is done I/O
+
+    def __init__(self):
+        self.roll = None
+
+    def _print(self, *args):
+        print(args[0])
+
+    def _input(self, *args):
+        print(args[0], 'n')
+        return 'n'
+
+class ParticipationTropyPlayer:
+
+    def __init__(self):
+        self.roll = None
+        self.name = None
+
+
+    def _print(self, *args):
+
+        msg =args[0]
+
+        if msg.startswith('You rolled'):
+            self.roll = [int(char) fo] #resimde var
+        print(args[0])
+
+    def _input(self, *args):
+        prompt = arg[0]
+
+        if prompt == 'Wanna play? ':
+            print(prompt, 'y')
+        return 'y'
+
+        # if prompt == 'Which would like to keep?  ':
+        #     score = self.game.calculate_score(self.roll)
+
+        if 1 in self.roll:
+            return '1'
+
+
+
+if __name__ == "__main__":
+    bot = LazyPlayer()
+    bot = ParticipationTropyPlayer()
+    game = Game(bot._print, bot._input)
+    bot.game = game
+    game.play(1)
